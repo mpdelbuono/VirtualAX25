0d81a0b367f1d80d13de64d2d825ccf39716e3ff
Matthew P. Del Buono (KG7UDH) <mpdelbuono@gmail.com>
2015-10-16 00:58:22 -0700

    Added GPL v3 license header to each file

--- VirtualAx25/Miniport.h
+++ VirtualAx25/Miniport.h
@@ -1,3 +1,20 @@
+// This file is part of the KG7UDH Virtual AX.25 NDIS Driver.
+//
+// The KG7UDH Virtual AX.25 NDIS Driver is free software: 
+// you can redistribute it and / or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The KG7UDH Virtual AX.25 NDIS Driver is distributed in 
+// the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this software. If not, see <http://www.gnu.org/licenses/>.
+
 /**
  * @file Miniport.h
  * Defines the Miniport class, which provides the NDIS Miniport handler function interface
